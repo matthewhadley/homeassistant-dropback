a42a477e64f67fc12161fe8e31ac4dc8b709acf6
matthewhadley <matthew.hadley@gmail.com>
2022-08-17 11:30:00 -0500

    add timestamps to log output

--- dropback/CHANGELOG.md
+++ dropback/CHANGELOG.md
@@ -1,3 +1,11 @@
-## [1.0.0] - 2022-08-16
+## [1.0.0] - 2022-08-17
+
 ### Added
+
+- Add timestamps to log output
+
+## [1.0.0] - 2022-08-16
+
+### Added
+
 - First release, with functionality to upload backups to Dropbox and optionally delete backups older than user configured days
--- dropback/run.sh
+++ dropback/run.sh
@@ -13,20 +13,27 @@ BACKUP_DIR="/backup"
 CONFIG_FILE="/uploader.conf"
 REFRESH_TOKEN_FILE="/config/719b45ef_dropback.conf"
 
+log() {
+    date +"%Y-%m-%d %T $*"
+}
+
+log "Initializing Dropback"
+
 if [ ! -e "$REFRESH_TOKEN_FILE" ]; then
-    echo "Requesting long lived refresh token..."
+    log "No stored Refresh Token found at $REFRESH_TOKEN_FILE"
+    log "Requesting long lived Refresh Token..."
     OAUTH_REFRESH_TOKEN=$(curl https://api.dropbox.com/oauth2/token \
     -d grant_type=authorization_code \
     -u $OAUTH_APP_KEY:$OAUTH_APP_SECRET \
     -d code=$OAUTH_ACCESS_CODE \
     --silent | jq -r .refresh_token)
 
-    echo "Storing encrypted Refresh Token at $REFRESH_TOKEN_FILE"
+    log "Storing encrypted Refresh Token at $REFRESH_TOKEN_FILE"
     echo $OAUTH_REFRESH_TOKEN | openssl aes-256-cbc -a -salt -pass pass:$OAUTH_APP_SECRET 2> /dev/null 1> $REFRESH_TOKEN_FILE
 fi
 
 if [ ! -e "$CONFIG_FILE" ]; then
-    echo "Using encrypted Refresh Token at $REFRESH_TOKEN_FILE"
+    log "Using encrypted Refresh Token at $REFRESH_TOKEN_FILE"
     OAUTH_REFRESH_TOKEN=$(cat $REFRESH_TOKEN_FILE | openssl aes-256-cbc -d -a -salt -pass pass:$OAUTH_APP_SECRET 2> /dev/null)
 
     echo "CONFIGFILE_VERSION=2.0" > "$CONFIG_FILE"
@@ -35,54 +42,50 @@ if [ ! -e "$CONFIG_FILE" ]; then
     echo "OAUTH_REFRESH_TOKEN=$OAUTH_REFRESH_TOKEN" >> "$CONFIG_FILE"
 fi
 
-echo -n "Validating Dropbox access... "
+log "Validating Dropbox access... "
 ./dropbox_uploader.sh -q -f $CONFIG_FILE space > /dev/null
 if [ $? -eq 0 ]; then
-   echo "OK"
+   log "Dropbox access OK"
 else
-   echo "ERROR"
+   log "ERROR validating Dropbox access. Please check configuration and restart."
    exit 1
 fi
 
-echo "Listening for input via stdin service call..."
+log "Listening for input via stdin service call..."
 
 # listen for input
 while read -r INPUT; do
     INPUT=${INPUT:1:-1}
-    echo "Received input: $INPUT"
+    log "Received input: $INPUT"
     if [[ "$INPUT" = "sync" ]]; then
 
         # find files older than X days, delete locally and delete on dropbox
         if [[ $DELETE_OLDER_THAN != "0" ]]; then
-            echo -n "Deleting local files older than $DELETE_OLDER_THAN day(s)"
-            if [[ $SYNC_DELETES = "true" ]]; then
-                echo -n " and syncing deletes to Dropbox"
-            fi
-            echo "..."
+            log "Deleting files older than $DELETE_OLDER_THAN day(s)"
             find /backup -maxdepth 1 -mtime +$(($DELETE_OLDER_THAN-1)) -type f -name "*.tar" -print0 |
             while IFS= read -r -d '' FILE; do
                 rm $FILE
-                echo "Deleted local file $FILE"
+                log "Deleted local file $FILE"
                 if [[ $SYNC_DELETES = "true" ]]; then
                     BASENAME=$(basename $FILE)
                     FILE_PATH=$(echo "$FOLDER/$BASENAME" | tr -s /)
-                    echo "Sync delete with Dropbox..."
+                    log "Sync delete with Dropbox..."
                     ./dropbox_uploader.sh -f $CONFIG_FILE delete "$FILE_PATH"
                 fi
             done
-            echo "Done with deletes"
+            log "Done with deletes"
         fi
 
         # find remaining files and upload to dropbox
-        echo "Uploading local files to Dropbox..."
+        log "Uploading local files to Dropbox..."
         ./dropbox_uploader.sh -s -f $CONFIG_FILE upload /backup/*.tar "$FOLDER"
-        echo "Done with uploads"
-        echo "Sync done"
+        log "Done with uploads"
+        log "Sync done"
 
     else
         # received undefined command
-        echo "Ignoring unknown input: $INPUT"
+        log "Ignoring unknown input: $INPUT"
     fi
 
-    echo "Listening for input via stdin service call..."
+    log "Listening for input via stdin service call..."
 done
