c25ec4f14c7508e7ae22910fa5eb5a45d8349c57
matthewhadley <matthew.hadley@gmail.com>
2024-12-19 12:33:44 -0600

    feat: generic build/publish

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ docker run \
     -v "$(pwd)":/data \
     homeassistant/aarch64-builder \
     --all \
-    --target dropback \
+    --target $(find . -type f -name "Dockerfile" -exec dirname {} \; | awk -F'/' '{print $NF}') \
     --docker-user $DOCKER_USER \
     --docker-password $DOCKER_PASSWORD
